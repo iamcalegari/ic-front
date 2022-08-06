40ebffa3648ce8e52608d5c89558925dabcd849d
iamcalegari <iamcalegari20@gmail.com>
2022-08-05 23:34:16 -0300

    resolve conflitos

--- src/components/basicos/if.jsx
+++
@@ -1,9 +0,0 @@
-/* eslint-disable import/no-anonymous-default-export */
-
-export default (props) => {
-  if (props.test) {
-    return props.children;
-  } else {
-    return false;
-  }
-};
--- src/data/produtos.js
+++
@@ -1,5 +0,0 @@
-export default [
-  { id: 1, nome: "Notebook", preco: 2000 },
-  { id: 2, nome: "Mouse", preco: 80 },
-  { id: 3, nome: "Monitor", preco: 500 },
-];
