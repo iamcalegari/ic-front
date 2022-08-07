07a8973f86c7abb5c984d73c4989a192240295ba
iamcalegari <iamcalegari20@gmail.com>
2022-08-07 13:26:45 -0300

    wip

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ import { resolve } from "path";
 
 const app = express();
 
-app.use("/", express.static(resolve(__dirname, "public")));
+app.use("/", express.static(resolve(__dirname, "./dist")));
 
 app.listen(process.env.PORT || 3000, (err) => {
   if (err) {
