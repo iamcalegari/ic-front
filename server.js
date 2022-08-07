782f807fb02b1334023dded5dfa5608f88e76809
iamcalegari <iamcalegari20@gmail.com>
2022-08-07 13:28:01 -0300

    wip

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
 import express from "express";
 import { resolve } from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+
+const __dirname = path.dirname(__filename);
 
 const app = express();
 
