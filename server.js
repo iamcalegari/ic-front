d45e17bd7c7bafba0c97510f7eff56b321e444a3
iamcalegari <iamcalegari20@gmail.com>
2022-08-07 13:06:14 -0300

    wip

--- server.js
+++ server.js
@@ -1,1 +1,14 @@
-console.log("chegou aqui");
+const express = require("express");
+const { resolve } = require("path");
+
+const app = express();
+
+app.use("/", express.static(resolve(__dirname, "./dist")));
+
+app.listen(process.env.PORT || 3000, (err) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(`Server is running on ${process.env.PORT || 3000}`);
+  }
+});
