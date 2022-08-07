ff5cffe7b31363a0108e937bb1fa1793f6d1497a
iamcalegari <iamcalegari20@gmail.com>
2022-08-07 13:29:15 -0300

    wip

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 import express from "express";
-import { resolve } from "path";
+import { resolve, dirname } from "path";
 import { fileURLToPath } from "url";
 
 const __filename = fileURLToPath(import.meta.url);
 
-const __dirname = path.dirname(__filename);
+const __dirname = dirname(__filename);
 
 const app = express();
 
