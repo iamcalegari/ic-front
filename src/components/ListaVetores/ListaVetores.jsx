8dc6bce43df8c95806741e922991910cd8ea3b05
iamcalegari <iamcalegari20@gmail.com>
2022-08-18 17:57:32 -0300

    wip

--- src/components/ListaVetores/ListaVetores.jsx
+++ src/components/ListaVetores/ListaVetores.jsx
@@ -11,7 +11,7 @@ import {
 const ListaVetores = ({ vetores, max, format }) => {
   const baseUrlApi = "https://ic-iot.herokuapp.com/api/vetores/";
   const listarVetores = (vetores) => {
-    return vetores.slice(0, +max).map(({ id, tamanho }, index) => {
+    return vetores.slice(0, +max).map((id, tamanho, index) => {
       return (
         <StyledAnchor href={`${baseUrlApi + id}/baixar/${format}`}>
           <ListItem key={id}>
